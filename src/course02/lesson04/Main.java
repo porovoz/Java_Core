84d1cc6e4f66106a6bc45df94abd6074b7924664
porovoz <falkon1@list.ru>
2022-12-21 18:14:57 +0300

    Homework 04 is starting.

---
+++ src/course02/lesson04/Main.java
@@ -0,0 +1,7 @@
+package course02.lesson04;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/course02/lesson04/transport/Transport.java
@@ -0,0 +1,4 @@
+package course02.lesson04.transport;
+
+public abstract class Transport {
+}
