8d828be79b5c8601d0743213bca62ba37ac38308
porovoz <falkon1@list.ru>
2022-12-23 15:49:30 +0300

    Homework 04 is done.

--- src/course02/lesson04/transport/Car.java
+++ src/course02/lesson04/transport/Car.java
@@ -1,4 +1,52 @@
 package course02.lesson04.transport;
 
-public class Car {
+public class Car extends Transport implements Competitor {
+
+    public Car() {
+        super("", "", 0);
+    }
+
+    public Car(String brand, String model, double engineVolume) {
+        super(brand, model, engineVolume);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public void startMoving() {
+        System.out.println("The car is starting to move");
+    }
+
+    @Override
+    public void stopMoving() {
+        System.out.println("The car is stopping");
+    }
+
+    @Override
+    public void pitStop() {
+        System.out.println("Your car should drive to a pit-stop");
+    }
+
+    @Override
+    public void getBestLapTime() {
+        System.out.println("Best car lap time is: ");
+    }
+
+    @Override
+    public void getMaximumSpeed() {
+        System.out.println("Maximum car speed is: ");
+    }
 }
--- src/course02/lesson04/transport/Truck.java
+++ src/course02/lesson04/transport/Truck.java
@@ -1,4 +1,51 @@
 package course02.lesson04.transport;
 
-public class Truck {
+public class Truck extends Transport implements Competitor {
+
+    public Truck() {
+        super("", "", 0);
+    }
+    public Truck(String brand, String model, double engineVolume) {
+        super(brand, model, engineVolume);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public void startMoving() {
+        System.out.println("The truck is starting to move");
+    }
+
+    @Override
+    public void stopMoving() {
+        System.out.println("The truck is stopping");
+    }
+
+    @Override
+    public void pitStop() {
+        System.out.println("Your truck should drive to a pit-stop");
+    }
+
+    @Override
+    public void getBestLapTime() {
+        System.out.println("Best truck lap time is: ");
+    }
+
+    @Override
+    public void getMaximumSpeed() {
+        System.out.println("Maximum truck speed is: ");
+    }
 }
